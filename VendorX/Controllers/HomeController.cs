4ca02df7693a157fb8771a862c168f56a31f066d
AsifZaman777 <eramasif16@gmail.com>
2026-01-14 21:56:55 +0600

    setup default route with identity

--- VendorX/Controllers/HomeController.cs
+++ VendorX/Controllers/HomeController.cs
@@ -1,14 +1,50 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using VendorX.Models;
 
 namespace VendorX.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public HomeController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
         {
-            return View();
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // Check if user is authenticated
+            if (_signInManager.IsSignedIn(User))
+            {
+                var user = await _userManager.GetUserAsync(User);
+                
+                if (user != null)
+                {
+                    // Redirect based on user role
+                    if (User.IsInRole("SuperAdmin"))
+                    {
+                        return RedirectToAction("Index", "Dashboard", new { area = "SuperAdmin" });
+                    }
+                    else if (User.IsInRole("ShopKeeper"))
+                    {
+                        return RedirectToAction("Index", "Home", new { area = "ShopKeeper" });
+                    }
+                    else if (User.IsInRole("Customer"))
+                    {
+                        return RedirectToAction("Index", "Home", new { area = "Customer" });
+                    }
+                }
+            }
+
+            // If not authenticated, redirect to login
+            return RedirectToAction("Login", "Account");
         }
 
         public IActionResult Privacy()
--- VendorX/Program.cs
+++ VendorX/Program.cs
@@ -84,7 +84,7 @@ app.MapControllerRoute(
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}")
+    pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
